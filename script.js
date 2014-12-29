6b40d5eef02e92cdff43ff04605211e99cc61d34
Carina <sem.carina@gmail.com>
2014-12-29 15:33:36 +0100

    classList

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-$ = function (elements) {
-	this.elements = document.querySelectorAll(elements);
-	//return document.querySelector(elemtns);
-	this.elements[0].className += " anotherClass";
-	//console.log(this.elements[0].className = 'myclass');
-}
+//document.addEventListener("DOMContentLoaded", function(event) {
+    //console.log("DOM fully loaded and parsed");
+	    $ = function (elements) {
+		this.elements = document.querySelectorAll(elements);
+		//return document.querySelector(elemtns);
+		//this.elements[0].className += " anotherClass";
+		console.log(this.elements[0].classList.add("myclass"));
+	}
+ // });
