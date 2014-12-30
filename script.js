a155068520f56f245b70c6ede458523a8bc94516
Carina <sem.carina@gmail.com>
2014-12-30 09:34:03 +0100

    prototypes

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-//document.addEventListener("DOMContentLoaded", function(event) {
-    //console.log("DOM fully loaded and parsed");
-	    $ = function (elements) {
-		this.elements = document.querySelectorAll(elements);
-		//return document.querySelector(elemtns);
-		//this.elements[0].className += " anotherClass";
-		console.log(this.elements[0].classList.add("myclass"));
-	}
- // });
+var obj = function (el) {
+	this.el = document.querySelectorAll(el);
+	//console.log(this.elements.length)
+
+	// for (i=0; i <= this.elements.length; i++) {
+	// 	console.log(this.elements[i]);
+	// }
+}
+
+obj.prototype.addClass = function (value){
+	//console.log(this.el);
+	this.el[0].classList.add(value);
+	return this;
+}
+
+function $ (el) {
+	return new obj(el);
+}
