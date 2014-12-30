cb12fb1147ebe4ce09a128f388583a8acd56b089
Carina <sem.carina@gmail.com>
2014-12-30 12:18:06 +0100

    first version

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 var obj = function (el) {
+	var dtp    = DOMTokenList.prototype;
+
 	this.el = document.querySelectorAll(el);
 	//console.log(this.elements.length)
 
-	// for (i=0; i <= this.elements.length; i++) {
-	// 	console.log(this.elements[i]);
-	// }
+	this.loop = function (elObj, method, val) {
+		for (i=0; i <= elObj.length-1; i++) {
+			elObj[i].classList[method](val);
+		}
+	}
 }
 
-obj.prototype.addClass = function (value){
-	//console.log(this.el);
-	this.el[0].classList.add(value);
-	return this;
+obj.prototype = {
+	addClass: function (value) {
+		this.loop(this.el, "add", value);
+		return this;
+	},
+	removeClass: function (value) {
+		this.loop(this.el, "remove", value);
+		return this;
+	},
+	toggleClass: function (value) {
+		this.loop(this.el, "toggle", value);
+		return this;
+	}
 }
 
 function $ (el) {
